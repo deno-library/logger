bf8826862995bf989b65ea2f1b0ac15b4cc8d6ba
fuxingZhang <502545703@qq.com>
2020-05-25 11:57:55 +0800

    add test file

--- test/test.ts
+++ test/test.ts
@@ -1,13 +1,53 @@
+
+const { test } = Deno;
+import {
+  assert,
+  assertEquals,
+  assertThrowsAsync
+} from "https://deno.land/std/testing/asserts.ts";
 import Logger from '../logger.ts';
 
-const logger = new Logger();
+const maxBytesError = "maxBytes cannot be less than 1";
+test(maxBytesError, async function () {
+  await assertThrowsAsync(
+    async () => {
+      const logger = new Logger();
+      logger.disableConsole();
+      await logger.initFileLogger('../log', {
+        maxBytes: 0
+      });
+    },
+    Error,
+    maxBytesError
+  );
+});
 
-logger.info('i am from info');
-logger.warn('i am from warn');
-logger.error('i am from error');
+const workWithError = "maxBackupCount must work with maxBytes";
+test(workWithError, async function () {
+  await assertThrowsAsync(
+    async () => {
+      const logger = new Logger();
+      logger.disableConsole();
+      await logger.initFileLogger('../log', {
+        maxBackupCount: 10
+      });
+    },
+    Error,
+    workWithError
+  );
+});
 
-await logger.initFileLogger('../log');
-
-logger.info('i am from info');
-logger.warn('i am from warn');
-logger.error('i am from error');
+const lessError = "maxBackupCount cannot be less than 1";
+test(lessError, async function (): Promise<void> {
+  try {
+    const logger = new Logger();
+    logger.disableConsole();
+    await logger.initFileLogger('../log', {
+      maxBytes: 10 * 2014,
+      maxBackupCount: 0
+    });
+    assert(false);
+  } catch (error) {
+    assertEquals(error.message, lessError);
+  }
+});
