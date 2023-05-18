c548532d3fe99b1f7ae7ff6c258465c864fe0a7b
UrielCh <uriel.chemouni@gmail.com>
2023-05-18 08:27:02 +0300

    move more deps to dev_deps.ts

--- dev_deps.ts
+++ dev_deps.ts
@@ -3,4 +3,4 @@ export {
     assertEquals,
     assertRejects,
   } from "https://deno.land/std@0.187.0/testing/asserts.ts";
-  
+export { build, emptyDir } from "https://deno.land/x/dnt@0.35.0/mod.ts";
