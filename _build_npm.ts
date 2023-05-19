5ae3f5b7aea4bd1a2c82a59d08df801a50912b21
UrielCh <uriel.chemouni@gmail.com>
2023-05-19 11:51:28 +0300

    merge build npm

--- _build_npm.ts
+++ _build_npm.ts
@@ -1,74 +1,55 @@
 // dnt deps can not be moved to dev_deps.ts
+import { PackageJsonObject } from "https://deno.land/x/dnt@0.35.0/lib/types.ts";
 import { build, emptyDir } from "https://deno.land/x/dnt@0.35.0/mod.ts";
-
-// deno run -A _build.ts 0.0.0;
-// cd npm; npm publish;
-// initial version will be v1.1.0
-if (!Deno.args[0]) {
-  console.error("Missing version number");
-  console.error("usage: deno run -A _build_npm.ts 0.0.0");
-  Deno.exit(-1);
-}
-
-type SPDXLicenseIdentifier = "MIT" | string; // could be more specific if you enumerate all valid SPDX identifiers.
-
-interface Person {
-  name: string;
-  email?: string;
-  url?: string;
-}
-
-interface Bugs {
-  url?: string;
-  email?: string;
-}
-
-interface PackageJson {
-  name: string;
-  version: string;
-  description?: string;
-  keywords?: string[];
-  homepage?: string;
-  bugs?: Bugs | string;
-  license?: SPDXLicenseIdentifier | 'UNLICENSED' | 'SEE LICENSE IN <filename>';
-  author?: Person | string;
-  contributors?: (Person | string)[];
-  main?: string;
-  types?: string;
-  scripts?: { [key: string]: string };
-  repository?: { type: string, url: string };
-  dependencies?: { [packageName: string]: string };
-  devDependencies?: { [packageName: string]: string };
-  peerDependencies?: { [packageName: string]: string };
-  bundleDependencies?: { [packageName: string]: string };
-  optionalDependencies?: { [packageName: string]: string };
-  engines?: { [engineName: string]: string };
-  os?: string[];
-  cpu?: string[];
-  private?: boolean;
-  /**
-   * rest of the fields
-   */
-  // deno-lint-ignore no-explicit-any
-  [propertyName: string]: any;
-}
+import * as pc from "https://deno.land/std@0.188.0/fmt/colors.ts";
 
 async function buildDnt() {
-  const version =  Deno.args[0];
-  const packageJson: PackageJson = {
+  let version = Deno.args[0];
+  const GITHUB_REF = Deno.env.get("GITHUB_REF");
+  const PKG_VERSION = Deno.env.get("PKG_VERSION");
+
+  if (!version) {
+    if (PKG_VERSION) {
+      console.log(`NPM_VERSION values is ${pc.green(PKG_VERSION)}`);
+      version = PKG_VERSION;
+    } else if (GITHUB_REF) {
+      // drop the ref/tag/ and the v prefix
+      version = GITHUB_REF.replace(/^.+\/[vV]?/g, "");
+      console.log(
+        `GITHUB_REF values is ${pc.green(
+          GITHUB_REF
+        )} will be used as version: ${pc.green(version)}`
+      );
+    }
+  }
+
+  if (!version) {
+    console.error("Missing version number");
+    console.error("usage: deno run -A _build_npm.ts 0.0.0");
+    Deno.exit(-1);
+  }
+  // allow only semver string
+  if (!version.match(/[\d]+\.[\d]+\.[\d]+/)) {
+    console.error(
+      `version number ${pc.green(
+        version
+      )} do not match Semantic Versioning syntax ${pc.green(
+        "major.minor.path"
+      )}`
+    );
+    Deno.exit(-1);
+  }
+
+  const packageJson: PackageJsonObject = {
     name: "@denodnt/logger",
-    author:
-      "zfx",
+    author: "zfx",
     license: "MIT",
     contributors: [
       "fuxing Zhang <fuxing.zhang@qq.com> (https://github.com/fuxingZhang)",
       "Uriel Chemouni <uchemouni@gmail.com> (https://uriel.ovh/)",
     ],
     description: "deno logger available for deno and NPM",
-    keywords: [
-      "logger",
-      "deno",
-    ],
+    keywords: ["logger", "deno"],
     private: false,
     homepage: "https://github.com/deno-library/logger",
     version,
@@ -81,29 +62,34 @@ async function buildDnt() {
     },
   };
 
-  try {
-    await emptyDir("./npm");
-    await build({
-      entryPoints: ["./mod.ts"],
-      outDir: "./npm",
-      shims: {
-        deno: true,
-      },
-      compilerOptions: {
-      },
-      package: packageJson,
-      // scriptModule: false,
-    });
-    Deno.copyFileSync("LICENSE", "npm/LICENSE");
-    let readme = Deno.readTextFileSync("README.md");
-    readme = readme.replaceAll(/https:\/\/deno.land\/x\/logger@v[0-9.]+\/(logger|mod)\.ts/g, '@denodnt/logger')
-    // readme = readme.replaceAll('https://deno.land/x/logger@v1.1.0/logger.ts', '@denodnt/logger')
-    readme = readme.replaceAll('logger for deno', `* [![NPM Version](https://img.shields.io/npm/v/@denodnt/logger.svg?style=flat)](https://www.npmjs.org/package/@denodnt/logger) Deno / NodeJS colorful logger colorful logger
-
-For Deno usage refer to [deno-logger doc](https://deno.land/x/logger@v${version})`)
-    Deno.writeTextFileSync("npm/README.md", readme);
-  } catch (e) {
-    console.error(e);
-  }
+  await emptyDir("./npm");
+  await build({
+    entryPoints: ["./mod.ts"],
+    outDir: "./npm",
+    shims: {
+      deno: true,
+    },
+    compilerOptions: {},
+    package: packageJson,
+    // scriptModule: false,
+  });
+  Deno.copyFileSync("LICENSE", "npm/LICENSE");
+  let readme = Deno.readTextFileSync("README.md");
+  readme = readme.replaceAll(
+    /https:\/\/deno.land\/x\/logger@v[0-9.]+\/(logger|mod)\.ts/g,
+    "@denodnt/logger"
+  );
+  readme = readme.replaceAll(
+    "https://deno.land/x/logger@v1.1.0/logger.ts",
+    "@denodnt/logger"
+  );
+  //  readme = readme.replaceAll(
+  //    "logger for deno",
+  //    `* [![NPM Version](https://img.shields.io/npm/v/@denodnt/logger.svg?style=flat)](https://www.npmjs.org/package/@denodnt/logger) Deno / NodeJS colorful logger colorful logger
+  //
+  //For Deno usage refer to [deno-logger doc](https://deno.land/x/logger@v${version})`
+  //  );
+  Deno.writeTextFileSync("npm/README.md", readme);
 }
+
 buildDnt();
