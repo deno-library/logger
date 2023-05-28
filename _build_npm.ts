9dd168bd2d5e894da8dfa241108f082dee565edb
UrielCh <uriel.chemouni@gmail.com>
2023-05-28 11:35:57 +0300

    use new dnt@0.36.0

--- _build_npm.ts
+++ _build_npm.ts
@@ -1,26 +1,23 @@
 // dnt deps can not be moved to dev_deps.ts
-import { PackageJsonObject } from "https://deno.land/x/dnt@0.35.0/lib/types.ts";
-import { build, emptyDir } from "https://deno.land/x/dnt@0.35.0/mod.ts";
+import { build, emptyDir, type PackageJson } from "https://deno.land/x/dnt@0.36.0/mod.ts";
 import * as pc from "https://deno.land/std@0.188.0/fmt/colors.ts";
 
-async function buildDnt() {
+export async function buildDnt() {
   let version = Deno.args[0];
   const GITHUB_REF = Deno.env.get("GITHUB_REF");
   const PKG_VERSION = Deno.env.get("PKG_VERSION");
 
   if (!version) {
     if (PKG_VERSION) {
-      console.log(`NPM_VERSION values is ${pc.green(PKG_VERSION)}`);
+      console.log(`NPM_VERSION values is "${pc.green(PKG_VERSION)}"`);
       version = PKG_VERSION;
     } else if (GITHUB_REF) {
       // drop the ref/tag/ and the v prefix
       version = GITHUB_REF.replace(/^.+\/[vV]?/g, "");
       console.log(
-        `GITHUB_REF values is ${
-          pc.green(
-            GITHUB_REF,
-          )
-        } will be used as version: ${pc.green(version)}`,
+        `GITHUB_REF values is ${pc.green(
+          GITHUB_REF
+        )} will be used as version: "${pc.green(version)}"`
       );
     }
   }
@@ -33,19 +30,16 @@ async function buildDnt() {
   // allow only semver string
   if (!version.match(/[\d]+\.[\d]+\.[\d]+/)) {
     console.error(
-      `version number ${
-        pc.green(version)
-      } do not match Semantic Versioning syntax ${
-        pc.green("major.minor.path")
-      }`,
+      `version number ${pc.green(version)} do not match Semantic Versioning syntax ${pc.green("major.minor.path")}`
     );
     Deno.exit(-1);
   }
 
-  const packageJson: PackageJsonObject = {
+  const packageJson: PackageJson = {
     name: "@denodnt/logger",
     author: "zfx",
     license: "MIT",
+    funding: "https://github.com/deno-library/logger?sponsor=1",
     contributors: [
       "fuxing Zhang <fuxing.zhang@qq.com> (https://github.com/fuxingZhang)",
       "Uriel Chemouni <uchemouni@gmail.com> (https://uriel.ovh/)",
@@ -68,13 +62,14 @@ async function buildDnt() {
   await build({
     entryPoints: ["./mod.ts"],
     outDir: "./npm",
+    test: true,
     shims: {
       deno: true,
     },
     compilerOptions: {},
     package: packageJson,
-    // scriptModule: false,
   });
+  // post build steps
   Deno.copyFileSync("LICENSE", "npm/LICENSE");
   let readme = Deno.readTextFileSync("README.md");
   readme = readme.replaceAll(
@@ -84,4 +79,6 @@ async function buildDnt() {
   Deno.writeTextFileSync("npm/README.md", readme);
 }
 
-buildDnt();
+if (import.meta.main) {
+  buildDnt();
+}
