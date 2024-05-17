26cce91bc33aa2bce6710d2da47a2f196bcc92a8
urielch <uriel.chemouni@gmail.com>
2024-05-17 13:41:01 +0200

    use JSR

--- deps.ts
+++ deps.ts
@@ -3,5 +3,7 @@ export {
   red,
   stripColor,
   yellow,
-} from "https://deno.land/std@0.189.0/fmt/colors.ts";
-export { writeAll } from "https://deno.land/std@0.189.0/streams/write_all.ts";
+} from "jsr:@std/fmt@0.225.1/colors";
+
+export { writeAll } from "jsr:@std/streams@0.213.0/write_all";
+// "https://deno.land/std@0.189.0/streams/write_all.ts";
